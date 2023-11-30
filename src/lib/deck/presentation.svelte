4b8608badc1156c7286715b6223ba35a62186496
MoritzSkrepek <skrepek.moritz@student.htlwrn.ac.at>
2023-11-30 09:44:11 +0100

    Trigger types

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -12,7 +12,10 @@
 <Slide id="table_of_contents">
 	<h3>Table Of Contents</h3>
 	<ol>
-
+		<li>Syntax PL/SQL</li>
+		<li>Syntax T/SQL</li>
+		<li>Vor-/Nachteile</li>
+		<li>Live Demo</li>
 	</ol>
 </Slide>
 
@@ -153,7 +156,7 @@
 </Slide>
 
 <!-- T-SQL Triggers -->
-<Slide id"syntax">
+<Slide id="syntax">
 	<Slide>
 		<h3>Syntax</h3>
 	</Slide>
@@ -163,73 +166,144 @@
     </Slide>
 
 	<!-- DML Trigger -->
-	<Slide>
+	<Slide animate style="display:flex; justify-content:center;">
 		<h3>DML Trigger</h3>
-		<Code>
-			<pre>
-                CREATE TRIGGER tr_dml_example
-                ON table_name
-                AFTER INSERT, UPDATE, DELETE
-                AS
-                -- Trigger logic here
-			</pre>
-		</Code>
+		<img
+		    src="dmltrigger.png"
+		    style="width: 125%"
+		/>
+		<Notes>
+          <h3>DML Trigger Syntax:</h3>
+          <p>
+            Ein DML (Data Manipulation Language) Trigger reagiert auf Änderungen in Daten einer Tabelle oder Sicht.
+          </p>
+          <ul>
+            <li>ON [ table | view ]: Specifies the triggering table or view.</li>
+            <li>[ FOR | AFTER | INSTEAD OF ]: Determines the trigger execution timing.</li>
+            <li>[ [ INSERT ] [ ; ] [ UPDATE ] [ ; ] [ DELETE ] ]: Defines the triggering events.</li>
+            <li>WITH APPEND: Allows appending multiple triggers to the same event.</li>
+            <li>NOT FOR REPLICATION: Prevents trigger execution during replication changes.</li>
+          </ul>
+        </Notes>
+	</Slide>
+
+	<Slide animate style="display:flex; justify-content:center;">
+	    <Code>
+	    {`
+	    -- DML Trigger Example
+        CREATE TRIGGER dbo.AfterUpdateTrigger
+        ON dbo.YourTable
+        AFTER UPDATE
+        AS
+        BEGIN
+          -- Your trigger logic here
+          PRINT 'Rows in YourTable were updated.';
+        END;
+	    `}
+	    </Code>
 	</Slide>
 
 	<!-- DDL Trigger -->
-	<Slide>
+	<Slide animate style="display:flex; justify-content:center;">
 		<h3>DDL Trigger</h3>
-		<Code>
-			<pre>
-                CREATE TRIGGER tr_ddl_example
-                ON DATABASE
-                FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
-                AS
-                -- Trigger logic here
-			</pre>
-		</Code>
+        <img
+		    src="ddltrigger.png"
+		    style="width: 125%"
+		/>
+		<Notes>
+          <h3>DDL Trigger Syntax:</h3>
+          <p>
+            Ein DDL (Data Definition Language) Trigger reagiert auf Strukturänderungen in der Datenbank.
+          </p>
+          <ul>
+            <li>ON [ ALL SERVER | DATABASE ]: Specifies whether the trigger is on the entire server or a specific database.</li>
+            <li>[ FOR | AFTER ] [ event_type | event_group ] [ ,...n ]: Defines triggering events or event groups.</li>
+            <li>WITH ENCRYPTION: Optionally encrypts the trigger text.</li>
+            <li>EXECUTE AS Clause: Sets the security context for trigger execution.</li>
+          </ul>
+        </Notes>
 	</Slide>
 
-	<!-- LOGON/LOGOFF Trigger -->
-	<Slide>
-		<h3>LOGON/LOGOFF Trigger</h3>
-		<Code>
-			<pre>
-                CREATE TRIGGER tr_logon_example
-                ON ALL SERVER
-                FOR LOGON
-                AS
-                -- Trigger logic here
-			</pre>
-		</Code>
+	<Slide animate style="display:flex; justify-content:center;">
+        <Code>
+        {`
+        -- DDL Trigger Example
+        CREATE TRIGGER ddlTriggerExample
+        ON DATABASE
+        FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
+        AS
+        BEGIN
+          -- Your trigger logic here
+          PRINT 'A table was created, altered, or dropped.';
+        END;
+        `}
+        </Code>
+    </Slide>
 
-		<Code>
-			<pre>
-                CREATE TRIGGER tr_logoff_example
-                ON ALL SERVER
-                FOR LOGOFF
-                AS
-                -- Trigger logic here
-			</pre>
-		</Code>
+	<!-- LOGON Trigger -->
+	<Slide animate style="display:flex; justify-content:center;">
+		<h3>LOGON Trigger</h3>
+        <img
+		    src="logontrigger.png"
+		    style="width: 125%"
+		/>
+		<Notes>
+          <h3>LOGON Trigger Syntax:</h3>
+          <p>
+            Ein LOGON Trigger reagiert auf Benutzeranmeldeereignisse auf der Serverebene.
+          </p>
+          <ul>
+            <li>ON ALL SERVER: Indicates the trigger is on the entire server.</li>
+            <li>[ FOR | AFTER ] LOGON: Determines the trigger execution timing for logon events.</li>
+            <li>WITH ENCRYPTION: Optionally encrypts the trigger text.</li>
+            <li>EXECUTE AS Clause: Sets the security context for trigger execution.</li>
+          </ul>
+        </Notes>
 	</Slide>
 
-	<!-- System Trigger for T-SQL -->
-	<Slide style="display:flex; position: absolute; top: 10%">
-		<h3>T-SQL</h3>
-		<p>System Trigger</p>
-		<div style="display:grid; gap: 2% 5%; grid: auto / auto auto">
-			<div>
-				<p>Event Clause</p>
-				<img src="tsql_event_clause.gif" alt="insert trigger here" />
-			</div>
-			<!-- Add other clauses for T-SQL system trigger here -->
-		</div>
-	</Slide>
+	<Slide animate style="display:flex; justify-content:center;">
+        <Code>
+        {`
+        -- LOGON Trigger Example
+        CREATE TRIGGER logonTriggerExample
+        ON ALL SERVER
+        AFTER LOGON
+        AS
+        BEGIN
+          -- Your trigger logic here
+          PRINT 'User logged in to the server.';
+        END;
+        `}
+        </Code>
+    </Slide>
+
+    <Slide>
+        <h3>Vor- und Nachteile von T/SQL</h3>
+        <div style="display: flex; justify-content: space-between;">
+            <div style="flex: 1;">
+                <h4>Vorteile (T/SQL)</h4>
+                <ul>
+                    <li>Breite Integration mit Microsoft-Produkten</li>
+                    <li>Transact-SQL-Sprachfunktionen</li>
+                    <li>Unterstützung von XML-Daten</li>
+                    <!-- Weitere Vorteile hinzufügen -->
+                </ul>
+            </div>
+            <div style="flex: 1;">
+                <h4>Nachteile (T/SQL)</h4>
+                <ul>
+                    <li>Plattformabhängig (Microsoft SQL Server)</li>
+                    <li>Begrenzte Unterstützung für einige PL/SQL-Funktionen</li>
+                    <li>Eingeschränkte Portabilität</li>
+                    <!-- Weitere Nachteile hinzufügen -->
+                </ul>
+            </div>
+        </div>
+    </Slide>
+
 </Slide>
 
-
-<Slide>
-
+<Slide id="Beispiele">
+    <h5>Live Demo</h5>
 </Slide>
 
