f3f25ad318108e12f7db57ea89073b1efe529505
if-loop69420 <j.sztavi@pm.me>
2023-11-30 21:44:42 +0100

    Added a background

--- src/lib/deck/slide.svelte
+++ src/lib/deck/slide.svelte
@@ -2,12 +2,16 @@
 	export let id: string | null = null
 	export let animate = false
 	export let restart = false
+	export let background: string = "https://media.itpro.co.uk/image/upload/v1570817004/itpro/2019/01/database.jpg"
+	export let backgroundOpacity: number = 0.5
 </script>
 
 <section
 	data-auto-animate-id={id}
 	data-auto-animate={animate || null}
 	data-auto-animate-restart={restart || null}
+	data-background-image={background}
+	data-background-opacity={backgroundOpacity}
 >
 	<slot />
 </section>
